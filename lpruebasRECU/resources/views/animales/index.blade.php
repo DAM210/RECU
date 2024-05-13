fa32ebdc238083f030ca0eb9a72771398067b967
DAM210 <lucia.ferrerasfernandez-marcote@iesmiguelherrero.com>
2024-05-13 18:12:33 +0200

    Hecho hasta la hoja 9. Falta completar store y update

--- lpruebasRECU/resources/views/animales/index.blade.php
+++ lpruebasRECU/resources/views/animales/index.blade.php
@@ -5,25 +5,25 @@
 @foreach ($animales as $animal)
 <div class="grid grid-cols-3 gap-2">
     <div class="px-5 py-8 border rounded border-green-500">
-        <img src="{{asset('assets/imagenesAnimales/'.$animal['imagen'])}}" alt="{{$animal['especie']}}">
+        <img src="{{asset('assets/imagenesAnimales/'.$animal->imagen)}}" alt="{{$animal->especie}}">
     </div>
     <div class="col-span-2 px-5 py-8 border rounded border-green-500">
         <h2 class="text-lg font-semibold">
-            <a href="{{route('animales.show',$animal['especie'])}}">
-                {{$animal['especie']}}
+            <a href="{{route('animales.show',$animal)}}">
+                {{$animal->especie}}
             </a>
         </h2>
 
         <ul>
-            <li>Peso: <span class="font-light">{{$animal['peso']}}</span> kg</li>
+            <li>Peso: <span class="font-light">{{$animal->peso}}</span> kg</li>
             <hr>
-            <li>Altura: <span class="font-light">{{$animal['altura']}}</span> cm</li>
+            <li>Altura: <span class="font-light">{{$animal->altura}}</span> cm</li>
             <hr>
-            <li>Fecha de nacimiento: <span class="font-light">{{$animal['fechaNacimiento']}}</span></li>
+            <li>Fecha de nacimiento: <span class="font-light">{{$animal->fechaNacimiento}}</span></li>
             <hr>
-            <li>Alimentación: <span class="font-light">{{$animal['alimentacion']}}</span></li>
+            <li>Alimentación: <span class="font-light">{{$animal->alimentacion}}</span></li>
             <hr>
-            <li>Descripción: <span class="font-light">{{$animal['descripcion']}}</span></li>
+            <li>Descripción: <span class="font-light">{{$animal->descripcion}}</span></li>
         </ul>
     </div>
 </div>
